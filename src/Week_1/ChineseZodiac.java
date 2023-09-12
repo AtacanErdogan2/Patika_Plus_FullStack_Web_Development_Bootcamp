69182e1572cbf924d39ca6db53b8323bf29c102f
AtacanErdogan <atacanerdogan2@gmail.com>
2023-09-12 17:33:15 +0300

    second commit

--- src/Week_1/ChineseZodiac.java
+++ src/Week_1/ChineseZodiac.java
@@ -8,7 +8,7 @@ public class ChineseZodiac {
 
         // Variable identification
         int year, zodiacNumber;
-        String zodiacSign;
+        String zodiacSign = "";
 
         // Retrieving data from the user
         System.out.print("Please enter your year of birth : ");
@@ -20,54 +20,43 @@ public class ChineseZodiac {
         switch (zodiacNumber) {
             case 0:
                 zodiacSign = "Monkey";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 1:
                 zodiacSign = "Rooster";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 2:
                 zodiacSign = "Dog";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 3:
                 zodiacSign = "Pig";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 4:
                 zodiacSign = "Rat";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 5:
                 zodiacSign = "Ox";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 6:
                 zodiacSign = "Tiger";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 7:
                 zodiacSign = "Rabbit";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 8:
                 zodiacSign = "Dragon";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 9:
                 zodiacSign = "Snake";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 10:
                 zodiacSign = "Horse";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
             case 11:
                 zodiacSign = "Goat";
-                System.out.println("Your Chinese zodiac sign: " + zodiacSign);
                 break;
         }
 
+        System.out.println("Your Chinese zodiac sign: " + zodiacSign);
 
     }
 }
